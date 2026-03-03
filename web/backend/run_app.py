c2e3d965696ae10bf30419ca37a9550ce5ade6ae
Samuel Rodriguez Matus <samuel@MacBook-Pro-de-Samuel.local>
2026-03-02 20:10:02 -0600

    feat: configure executable to launch directly into dashboard

--- web/backend/run_app.py
+++ web/backend/run_app.py
@@ -6,7 +6,7 @@ from main import app
 
 def open_browser():
     time.sleep(2)  # Wait for the server to start
-    webbrowser.open("http://127.0.0.1:8000")
+    webbrowser.open("http://127.0.0.1:8000/dashboard")
 
 if __name__ == "__main__":
     threading.Thread(target=open_browser, daemon=True).start()
